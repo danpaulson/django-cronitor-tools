1a18609fb49dc5022a751f2ebfb3a501148b54a9
Dan Paulson <danpaulson@gmail.com>
2023-12-21 14:04:19 -0600

    Fix import

--- cronitor_tools/management/commands/cronitor-set-monitors.py
+++ cronitor_tools/management/commands/cronitor-set-monitors.py
@@ -3,7 +3,7 @@ import cronitor
 from django.core.management.base import BaseCommand
 from django.conf import settings
 
-from cronitor_config_decorator import discover_cronitor_tasks
+from cronitor_tools import discover_cronitor_tasks
 
 
 class Command(BaseCommand):
